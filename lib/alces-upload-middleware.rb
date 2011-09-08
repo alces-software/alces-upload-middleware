33b2b7193b721e525a8f07cfadd6812286edb288
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-09-08 23:56:32 +0100

    Initial commit to alces-upload-middleware.

---
+++ lib/alces-upload-middleware.rb
@@ -0,0 +1,9 @@
+require "alces-upload-middleware/version"
+
+module Alces
+  module Upload
+    module Middleware
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/alces-upload-middleware/version.rb
@@ -0,0 +1,7 @@
+module Alces
+  module Upload
+    module Middleware
+      VERSION = "0.0.1"
+    end
+  end
+end
